fc5f71b1006522d8f058d31d9de8e69709f075d5
Bruce Rojas <rojash1302@gmail.com>
2024-07-30 17:52:10 -0500

    crando index y enlazando bd

---
+++ src/main/java/me/bruce/App/controllerIndex.java
@@ -0,0 +1,17 @@
+package me.bruce.App;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class controllerIndex {
+    @GetMapping("/")
+    public String inicio()
+    {
+        log.info("Ejecutando el controlador");
+
+        return "index";
+    }
+}
